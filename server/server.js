1007e791afc03723550a0d09a208b4767e491eb7
charlesonunze <charlesonunze2@gmail.com>
2018-04-07 13:24:53 +0100

    SocketIO integration

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log(`New user connected...`);
+
+  socket.on('disconnect', () => {
+    console.log(`client was disconnected...`);
+  });
+});
 
 app.get('/', (req, res) => {
   res.render('index.html');
 });
 
-app.listen(port, function() {
+server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
